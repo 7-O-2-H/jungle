d44036b54ae73dac097beffed3142e69068cc31e
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-05-10 17:49:50 -0400

    corrected product tests, added docs page for images, updated README

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,33 +5,33 @@ RSpec.describe Product, type: :model do
 
     it 'is valid with all the right values' do
       @category = Category.new
-      params = { name: 'Bird Paradise', description: 'Tropical plant', quantity: 20, category: @category, price_cents: 6000 }
+      params = { name: 'Monstera delicosa', description: 'split-leaf philodendron', quantity: 20, category: @category, price_cents: 6000 }
       expect(Product.new(params)).to be_valid
     end
   end
 
   it 'is invalid with the name missing' do
     @category = Category.new
-    params = { description: 'Tropical plant', quantity: 20, category: @category, price_cents: 6000 }
+    params = { description: 'split-leaf philodendron', quantity: 20, category: @category, price_cents: 50000 }
     expect(Product.new(params)).to_not be_valid
   end
 
   it 'is invalid with the quantity missing' do
     @category = Category.new
-    params = { name: 'Bird Paradise', description: 'Tropical plant', category: @category, price_cents: 6000 }
+    params = { name: 'Monstera delicosa', description: 'split-leaf philodendron', category: @category, price_cents: 50000 }
     expect(Product.new(params)).to_not be_valid
   end
 
   it 'is invalid with the category missing' do
     @category = Category.new
-    params = { name: 'Bird Paradise', description: 'Tropical plant', quantity: 20, price_cents: 6000 }
+    params = { name: 'Monstera delicosae', description: 'split-leaf philodendron', quantity: 20, price_cents: 50000 }
     expect(Product.new(params)).to_not be_valid
   end
 
   it 'is invalid with the price missing' do
     @category = Category.new
-    params = { name: 'Bird Paradise', description: 'Tropical plant', quantity: 20, category: @category }
+    params = { name: 'Monstera delicosa', description: 'split-leaf philodendron', quantity: 20, category: @category }
     expect(Product.new(params)).to_not be_valid
   end
-  
+
 end
